e39baefb6664f877e394bef04c4c7d0ee98233b5
tatsukiKikugawa <tk802@georgetown.edu>
2020-03-18 18:08:56 -0400

    Simplify USD formatting

--- app/shopping_mini.py
+++ app/shopping_mini.py
@@ -2,6 +2,12 @@
 from datetime import datetime
 import os
 
+
+#Goal 1: simplify USD formatting
+#Goal 2: simplify receipt printing / file writing
+#Goal 2: simplify receipt printing / file writing
+
+
 checkout_at = datetime.now().strftime("%M/%d/%Y %I:%m %p")
 
 selected_products = [
@@ -11,6 +17,18 @@ selected_products = [
     {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
 ] # FYI: for the purposes of this exercise, you won't need to modify this list at all
+
+def to_usd(my_price):
+    """
+    Converts a numeric value to usd-formatted string, for printing and display purposes.
+    Source: https://github.com/prof-rossetti/intro-to-python/blob/master/notes/python/datatypes/numbers.md#formatting-as-currency
+    Param: my_price (int or float) like 4000.444444
+    Example: to_usd(4000.444444)
+    Returns: $4,000.44
+    """
+    return f"${my_price:,.2f}"
+
+
 
 now = datetime.now()
 
@@ -22,11 +40,11 @@ print("---------")
 print("CHECKOUT AT: " + str(now.strftime("%Y-%M-%d %H:%m:%S")))
 print("---------")
 for p in selected_products:
-    print("SELECTED PRODUCT: " + p["name"] + "   " + '${:.0f}'.format(p["price"]))
+    print("SELECTED PRODUCT: " + p["name"] + "   " + to_usd(p["price"]))
 print("---------")
-print(f"SUBTOTAL: {subtotal:,.2f}")
-print(f"TAX: {(subtotal * 0.0875):.2f}")
-print(f"TOTAL: {((subtotal * 0.0875) + subtotal):.2f}")
+print(f"SUBTOTAL: {to_usd(subtotal)}")
+print(f"TAX: {to_usd(subtotal * 0.0875)}")
+print(f"TOTAL: {to_usd((subtotal * 0.0875) + subtotal)}")
 print("---------")
 print("THANK YOU! PLEASE COME AGAIN SOON!")
 print("---------")
